d94609dc4ccc53e2d6ab17b55ce5ad0a7a7d6f59
ChengYeh Wang <cywang.eda@gmail.com>
2020-04-15 21:47:07 +0800

    multiple cpu / counter

--- perf_core.cpp
+++ perf_core.cpp
@@ -9,59 +9,33 @@
 #include<linux/perf_event.h>
 #include<linux/hw_breakpoint.h>
 int main() {
-	struct perf_event_attr pe;
-	memset(&pe, 0, sizeof(struct perf_event_attr));
-	pe.type = PERF_TYPE_HARDWARE;
-	pe.size = sizeof(struct perf_event_attr);
-	pe.config = PERF_COUNT_HW_CPU_CYCLES;
-	//pe.config = PERF_COUNT_HW_INSTRUCTIONS;
-	//pe.freq = 0;
-	pe.sample_freq = 1000;
-	pe.sample_type = PERF_SAMPLE_TIME;
-	pe.read_format = PERF_FORMAT_TOTAL_TIME_ENABLED;
-	pe.disabled = 1;
-	pe.exclude_kernel = 1;
-	pe.exclude_hv = 1;
-	pe.mmap = 1;
-	int fd;
-	int pid = 0; // this process
-	int cpu = -1; // all cpu
-	fd = syscall(__NR_perf_event_open, &pe, pid, cpu, -1, 0);
-	if (fd == -1) {
-		printf("can not open perf by syscall");
-		exit(0);
-	}
-	int PageSize = 0x1000;
-	int DataSize = PageSize * 2;
-	void *addr = mmap(NULL, PageSize * 3 , PROT_READ,
-                       MAP_SHARED, fd, 0);
-	if (addr == MAP_FAILED) {
-		printf("can not map by mmap");
-		exit(0);
-	}
-	ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-        ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
-	sleep(1);
-        ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
-	perf_event_mmap_page *mmap = reinterpret_cast<struct perf_event_mmap_page*>(addr);
-	unsigned long long *base = reinterpret_cast<unsigned long long *>(addr + PageSize);
-	printf("%llx %llx\n",addr, base);
-
-	for(int i=0;i<10;i++) {
-		
-		struct packet{
-			long long value;
-			long long time;
-		} data;
-		data.time = 0;
-		data.value = 0;
-		struct perf_event_mmap_page a;
-        	//int re = read(fd, &data, sizeof(struct packet));
-		int re = 0;
-		data.value = base[2*i];
-		data.time = base[2*i+1];
-		printf("re %d time %lld value %lld\n",
-			re, data.time, data.value);
-	}
+	int cpu = 8;
+	int counter = 6;
+	int sample = 0x1000;
+	struct perf_event_attr pe[cpu][counter];
+	int fd[cpu][counter];
+	for (int i=0 ; i<cpu ; i++)
+		for (int j=0 ; j<counter ; j++)
+		{
+			perf_event_attr& ref = pe[i][j];
+			memset(& ref, 0, sizeof(struct perf_event_attr));
+			ref.type = PERF_TYPE_HARDWARE;
+			ref.config = PERF_COUNT_HW_CPU_CYCLES;
+			fd[i][j] = syscall(__NR_perf_event_open, &pe, 0, i, -1, 0);
+			if (fd[i][j] == -1) {
+				printf("can not open perf by syscall");
+				exit(0);
+			}
+		}
+	unsigned long long data[sample][cpu][counter];
+	for (int k=0 ; k<sample ; k++)
+		for (int i=0 ; i<cpu ; i++)
+			for (int j=0 ; j<counter ; j++)
+			{
+				int re = read(fd[i][j], &(data[k][i][j]), sizeof(unsigned long long));
+			}
+	FILE *writer = fopen("mini_perf.bin", "wb");
+	fwrite(data, sizeof(unsigned long long),sample * cpu * counter, writer);
+	fclose(writer);
 	return 0;
 }
