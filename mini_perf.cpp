279cbcca82de6ae329887e410bbe8a431ac98f8c
chengyehwang <cywang.eda@gmail.com>
2021-06-27 00:54:27 +0800

    get id from gen table

--- mini_perf.cpp
+++ mini_perf.cpp
@@ -8,7 +8,6 @@
 #include<linux/perf_event.h>
 
 using namespace std;
-
 #define ATRACE_MESSAGE_LEN 256
 int     atrace_marker_fd = -1;
 
@@ -27,33 +26,56 @@ inline int trace_counter(const char *name, const int value)
 }
 int main() {
     trace_init();
+    #define EVENT_TYPE_TABLE_ENTRY(NAME, TYPE, ID, COM, S) {.id = ID, .name = NAME, .com = COM},
+    struct event_t {
+        int id;
+        const char *name;
+        const char *com;
+    };
+    event_t event_name[] = {
+        #include "event_type_table.h"
+        {.id = 0, .name = "", .com = ""}
+    };
+    //for(int i=0;i<256;i++)
+    //    printf("%x ",(unsigned long long) (event_name[i]));
+
     int cpu = 1;
     int group = 2;
     int counter = 2;
     int sample = 0x1000;
     struct perf_event_attr pe[cpu][group][counter];
     int fd[cpu][group][counter];
-    int group_counter[group][counter] = {
-        {
-            PERF_COUNT_HW_CPU_CYCLES,
-            PERF_COUNT_HW_INSTRUCTIONS
-        },
-        {
-            PERF_COUNT_HW_CACHE_MISSES,
-            PERF_COUNT_HW_BRANCH_MISSES
-        }
-            //PERF_COUNT_HW_BUS_CYCLES
-    };
     char group_name[group][counter][20] = {
         {
-            "cpu_cycles",
-            "instructions"
+            "instructions",
+            "cache_misses"
         },
         {
-            "cache_misses",
+            "instructions",
             "branch_misses"
         }
     };
+    int group_counter[group][counter]={0};
+    for (int group_i=0;group_i<group;group_i++)
+    {
+        for (int count_i=0;count_i<counter;count_i++)
+        {
+            for (int i=0;;i++)
+            {
+                if (event_name[i].id==0)
+                    break;
+                if (strcmp(group_name[group_i][count_i], event_name[i].name)==0)
+                {
+                    group_counter[group_i][count_i] = event_name[i].id;
+                }
+            }
+            if (group_counter[group_i][count_i] == 0)
+            {
+                printf("%s is missed\n",group_name[group_i][count_i]);
+                return 0;
+            }
+        }
+    }
     for (int cpu_i=0 ; cpu_i<cpu ; cpu_i++)
     {
         for (int group_i=0 ; group_i < group ; group_i++)
@@ -64,8 +86,9 @@ int main() {
                 perf_event_attr& ref = pe[cpu_i][group_i][count_i];
                 memset(& ref, 0, sizeof(struct perf_event_attr));
                 ref.type = PERF_TYPE_HARDWARE;
-                ref.read_format = PERF_FORMAT_GROUP;
+                ref.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_TOTAL_TIME_RUNNING;
                 ref.config = group_counter[0][count_i];
+                ref.freq = 1;
                 fd[cpu_i][group_i][count_i] = syscall(__NR_perf_event_open, &ref, -1, cpu_i, fd_prev, 0);
                 fd_prev = fd[cpu_i][group_i][0];
                 if (fd[cpu_i][group_i][count_i] == -1) {
@@ -75,7 +98,7 @@ int main() {
             }
         }
     }
-    unsigned long long data[sample][cpu][group][counter+1];
+    unsigned long long data[sample][cpu][group][counter+2];
     for (int k=0 ; k<sample ; k++)
     {
         usleep(1000);
@@ -88,11 +111,11 @@ int main() {
         for (int i=0 ; i<cpu ; i++)
             for (int j=0 ; j<group ; j++)
             {
-                int re = read(fd[i][j][0], &(data[k][i][j][0]), (counter+1) * sizeof(unsigned long long));
+                int re = read(fd[i][j][0], &(data[k][i][j][0]), (counter+2) * sizeof(unsigned long long));
             }
     }
     FILE *writer = fopen("mini_perf.data", "wb");
-    fwrite(data, sizeof(unsigned long long),sample * cpu * group * (counter+1), writer);
+    fwrite(data, sizeof(unsigned long long),sample * cpu * group * (counter+2), writer);
     fclose(writer);
     writer = fopen("mini_perf.head", "w");
     for (int cpu_i = 0 ; cpu_i < cpu ; cpu_i++)
@@ -100,6 +123,7 @@ int main() {
         for (int group_i=0 ; group_i<group ; group_i++)
         {
             fprintf(writer, "count: group%d_cpu%d\n",group_i,cpu_i);
+            fprintf(writer, "count: time_cpu%d\n",cpu_i);
             for(int count_i=0 ; count_i<counter ; count_i++)
             {
                 fprintf(writer, "count: %s_cpu%d\n",group_name[group_i][count_i],cpu_i);
