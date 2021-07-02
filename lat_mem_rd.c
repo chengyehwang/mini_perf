a594ce805aaa15c5233cb76d4428b01699fc2a39
chengyehwang <cywang.eda@gmail.com>
2021-07-02 14:57:15 +0800

    fix time

--- lat_mem_rd.c
+++ lat_mem_rd.c
@@ -125,12 +125,12 @@ loads(size_t len, size_t range, size_t stride,
 	result = stop(0, 0);
 	int div = state.npages * state.nlines;
 	double latency = result / (repeat*100) * 1000;
-	fprintf(stderr, "range: %d, div: %d, count: %d, time: %.3f ns\n", range, div, repeat*100, latency);
+	fprintf(stderr, "range: %9d, div: %7d, count: %10d, time: %2.3f ns\n", range, div, repeat*100, latency);
 
-	if (latency > 10) {
-	scale = 10;
-	} else if (latency > 100) {
+	if (latency > 100) {
 	scale = 100;
+	} else if (latency > 10) {
+	scale = 10;
 	}
 }
 
